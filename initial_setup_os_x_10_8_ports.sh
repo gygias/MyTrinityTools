1f6de848bb0ad292af4a21f80c90d16d2dcfe6c5
gygias <gygias@users.noreply.github.com>
2012-12-10 00:30:38 -0800

    no comment

--- initial_setup_os_x_10_8_ports.sh
+++ initial_setup_os_x_10_8_ports.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ ! "${PWD##*/}" = "build" ]; then
-    echo "run me from <srcroot>/build"
+    /bin/echo "run me from <srcroot>/build"
     exit 1
 fi
 
-cmake ../ -GXcode \
+/opt/local/bin/cmake ../ -GXcode \
     -DMYSQL_ADD_INCLUDE_PATH=/opt/local/include/mysql5/mysql \
     -DREADLINE_INCLUDE_DIR=/opt/local/include \
     -DREADLINE_LIBRARY=/opt/local/lib/libreadline.dylib \
@@ -14,17 +14,17 @@ cmake ../ -GXcode \
     -DPREFIX=/opt/trinitycore \
     -DMYSQL_LIBRARY=/opt/local/lib/mysql5/mysql/libmysqlclient_r.dylib
 
-echo "Dropping previous db…"
+/bin/echo "Dropping previous db…"
 /opt/local/bin/mysql5 -u root -e "drop database world"
 /opt/local/bin/mysql5 -u root -e "drop database characters"
 /opt/local/bin/mysql5 -u root -e "drop database auth"
 
-echo "Creating schema…"
+/bin/echo "Creating schema…"
 /bin/cat ../sql/create/create_mysql.sql | /opt/local/bin/mysql5 -u root
 /bin/cat ../sql/base/auth_database.sql | /opt/local/bin/mysql5 -u root auth
 /bin/cat ../sql/base/characters_database.sql | /opt/local/bin/mysql5 -u root characters
 
-echo "Importing TDB…"
+/bin/echo "Importing TDB…"
 /bin/cat ../../dbs/TDB_full_434.02_2012_08_30.sql | /opt/local/bin/mysql5 -u root world
 
 echo "Importing updates…"
@@ -32,4 +32,6 @@ echo "Importing updates…"
 /usr/bin/find ../sql/updates/characters -exec bash -c "echo {}; cat \"{}\" | mysql5 -u root characters" \;
 /usr/bin/find ../sql/updates/auth -exec bash -c "echo {}; cat \"{}\" | mysql5 -u root auth" \;
 
-xcodebuild -target install
+if [ "$1" = "build" ]; then
+    /usr/bin/xcodebuild -target install
+fi
